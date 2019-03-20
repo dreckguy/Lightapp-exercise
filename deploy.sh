04d1a25324ef1f4fc294493c08a53cd964ecda1d
Ophirus Magnivus <dreckguy@gmail.com>
2019-03-20 16:09:45 +0200

    scp

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $MACHINE_KEY > MACHINE_KEY.pem
-ssh -i MACHINE_KEY.pem -o "StrictHostKeyChecking no" $MACHINE_USER@$MACHINE_ADDRESS ls
+scp -i dashboard.pem -o "StrictHostKeyChecking no" docker-compose.yml MACHINE_USER@MACHINE_ADDRESS:~/
 rm -f MACHINE_KEY.pem 
